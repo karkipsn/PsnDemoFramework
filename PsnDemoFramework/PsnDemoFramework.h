1c28431965d7c0b99c087c0cf39b99c71d480106
Poshan <poshan@inficare.net>
2020-05-16 20:50:45 +0545

    psn second commit

---
+++ PsnDemoFramework/PsnDemoFramework.h
@@ -0,0 +1,19 @@
+//
+//  PsnDemoFramework.h
+//  PsnDemoFramework
+//
+//  Created by Poshan Karki on 5/16/20.
+//  Copyright © 2020 Inficare. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PsnDemoFramework.
+FOUNDATION_EXPORT double PsnDemoFrameworkVersionNumber;
+
+//! Project version string for PsnDemoFramework.
+FOUNDATION_EXPORT const unsigned char PsnDemoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PsnDemoFramework/PublicHeader.h>
+
+
---
+++ PsnDemoFramework/Source/TestFile.swift
@@ -0,0 +1,32 @@
+//
+//  TestFile.swift
+//  PsnDemoFramework
+//
+//  Created by Poshan Karki on 5/16/20.
+//  Copyright © 2020 Inficare. All rights reserved.
+//
+
+import Foundation
+
+open class TestFile {
+    
+    public init(){}
+            
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func multiply(a: Int, b: Int) -> Int {
+         return a * b
+    }
+    
+    public func subtract(a: Int, b: Int) -> Int {
+         return a - b
+    }
+    
+    public func divide(a: Int, b: Int) -> Int {
+         return a / b
+    }
+    
+}
+
